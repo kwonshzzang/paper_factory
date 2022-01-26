f092feec5da185c230cef8e462ca53a4a4762977
love0042 <love0042@gmail.com>
2022-01-26 13:31:40 +0900

    WebController 매핑

--- src/main/java/kr/re/etri/paper/web/WebRestController.java
+++ src/main/java/kr/re/etri/paper/web/WebRestController.java
@@ -1,5 +1,10 @@
 package kr.re.etri.paper.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class WebRestController {
 
 }
